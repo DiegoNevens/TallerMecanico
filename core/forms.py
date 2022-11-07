7a0bd461d2844e449d51e19b1990bf7ce9e0ac04
diego <di.nevens@duocuc.cl>
2022-11-07 19:54:55 -0300

    1er Commit

---
+++ core/forms.py
@@ -0,0 +1,34 @@
+from dataclasses import fields
+from django import forms
+from django.forms import ModelForm
+from .models import Contacto
+from .models import Trabajo
+from .models import Carro
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class ContactoForm(ModelForm):
+    class Meta:
+        model = Contacto
+        fields = ['rut','nombre','correo','mensaje','categoria']
+        
+        
+
+class TrabajoForm(ModelForm):
+    class Meta:
+        model = Trabajo
+        fields = ['diagnostico', 'mecanico', 'fecha', 'materiales', 'img', 'categoria']
+
+
+class CustomUserCreationForm(UserCreationForm):
+     
+     class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+        
+class CarroForm(ModelForm):
+    class Meta:
+        model = Carro
+        fields = ["rut","destinatario","monto","fecha"]
+        
